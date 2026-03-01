e42c6e9ac073e38cd58e456a75c73bf12be871e1
June Tate-Gans <june@theonelab.com>
2026-03-01 11:59:39 -0600

    Adding license header

--- endpoint.go
+++ endpoint.go
@@ -1,3 +1,22 @@
+// Copyright 2026, Retrocomp Networks Foundation, Inc.
+//
+// This file is part of RCN endpoint, a user-facing program to make
+// management of an RCN endpoint easy.
+//
+// librcn-api is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see
+// <https://www.gnu.org/licenses/>.
+
 package main
 
 import (
